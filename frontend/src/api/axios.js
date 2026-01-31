030f17c081b5277593b4b9c68f841da9ef35c0cc
HarshitRaj2712 <panditjee2712@gmail.com>
2026-01-31 12:45:17 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:5000';
 
 const api = axios.create({
-  baseURL: API_BASE + '/api',
+  baseURL: API_BASE ,
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true // IMPORTANT: allows sending/receiving cookies (refresh token)
 });
